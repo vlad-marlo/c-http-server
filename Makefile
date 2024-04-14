0e51e321d934e86ffe372cbe4d3d5b547fed7a21
Vlad Marlo <vlad.marlo@ya.ru>
2024-04-14 22:30:21 +0300

    added bound to address

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
 .PHONY: build
 build:
 	gcc -o webserver.o server.c
+
+
+.PHONY: run
+run: build
+	./webserver.o
+
+
+DEFAULT_GOAL := build
--- server.c
+++ server.c
@@ -4,6 +4,8 @@
 #include <netinet/tcp.h>
 #include <stdio.h>
 
+#define PORT 8080
+
 int main()
 {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -13,5 +15,20 @@ int main()
         return 1;
     }
     printf("socket created successfully\r\n");
+
+    struct sockaddr_in host_addr;
+    int host_addr_len = sizeof(host_addr);
+
+    host_addr.sin_family = AF_INET;
+    host_addr.sin_port = htons(PORT);
+    host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sockfd, (struct sockaddr *)&host_addr, host_addr_len) != 0)
+    {
+        perror("webserver (bind)");
+        return 1;
+    }
+    printf("socket successfully bound to address\r\n");
+
     return 0;
 }
