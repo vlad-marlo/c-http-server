b570a9cc37b6a99336f6e4b893a193cb2b874daf
Vlad Marlo <vlad.marlo@ya.ru>
2024-04-14 23:00:52 +0300

    finished v1 server

--- server.c
+++ server.c
@@ -1,13 +1,21 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #define PORT 8080
+#define BUFFER_SIZE 1024
 
 int main()
 {
+    char buffer[BUFFER_SIZE];
+    char resp[] = "HTTP/1.0 200 OK\r\n"
+                  "Server: webserver-c\r\n"
+                  "Content-type: text/html\r\n\r\n"
+                  "<html>hello, world</html>\r\n";
+
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) 
     {
@@ -29,6 +37,42 @@ int main()
         return 1;
     }
     printf("socket successfully bound to address\r\n");
+
+    if (listen(sockfd, SOMAXCONN) != 0) 
+    {
+        perror("webserver (listen)");
+        return 1;
+    }
+    printf("server listening for connections\r\n");
+
+    for (;;) 
+    {
+        int newsockfd = accept(sockfd,
+                               (struct sockaddr *)&host_addr,
+                               (socklen_t *)&host_addr_len);
+        if (newsockfd < 0) 
+        {
+            perror("webserver (accept)");
+            continue;
+        }
+        printf("connection accepted\r\n");
+
+        int valread = read(newsockfd, buffer, BUFFER_SIZE);
+        if (valread < 0)
+        {
+            perror("webserver (read)");
+            continue;
+        }
+
+        int valwrite = write(newsockfd, resp, strlen(resp));
+        if (valwrite < 0) 
+        {
+            perror("webserver (write)");
+            continue;
+        }
+
+        close(newsockfd);
+    }
 
     return 0;
 }
